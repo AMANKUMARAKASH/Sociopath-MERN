049a862233a92c543a9823f7c866483997951b30
AMANKUMARAKASH <89243224+AMANKUMARAKASH@users.noreply.github.com>
2024-02-24 08:35:33 +0530

    vercel

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -46,7 +46,7 @@ import {
         formData.append("picturePath", image.name);
       }
   
-      const response = await fetch(`http://localhost:3001/posts`, {
+      const response = await fetch(`https://sociopath-mern-api.vercel.app//posts`, {
         method: "POST",
         headers: { Authorization: `Bearer ${token}` },
         body: formData,
