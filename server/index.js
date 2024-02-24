794ee29c116849e5d4078992f3f853c7b171307d
AMANKUMARAKASH <89243224+AMANKUMARAKASH@users.noreply.github.com>
2024-02-24 08:44:58 +0530

    vercel

--- server/index.js
+++ server/index.js
@@ -32,11 +32,12 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors(
     {
-        origin: 'https://yourfrontenddomain.com',
+        origin: 'https://sociopath-mern.vercel.app/',
         methods: ['GET', 'POST'],
         credentials: true // Allow credentials (cookies, HTTP authentication) to be included in cross-origin requests
     }
 ));
+
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 /*File Storage */
